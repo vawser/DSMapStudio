8ae4a071bc1df5b47dd7f8af81abe5ee4eaeb6af
Vawser <ben.rouncefield@gmail.com>
2025-05-25 14:50:43 +0100

    Clean Up
    - Removed the duplicate Smithbox files from the merge

--- src/Smithbox.Program/Editors/TextEditor/Enums/SearchFilterType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace StudioCore.Editors.TextEditor;
-
-public enum SearchFilterType
-{
-    [Display(Name = "Primary Category")] PrimaryCategory = 0,
-    [Display(Name = "All Categories")] AllCategories = 1,
-}
--- src/Smithbox.Program/Editors/TextEditor/Enums/TextContainerType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace StudioCore.Editors.TextEditor;
-
-public enum TextContainerType
-{
-    [Display(Name = "Loose")] Loose,
-    [Display(Name = "BND")] BND
-}
